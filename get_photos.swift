4e37d290c3072ca8071239d7b0758c45873f2852
Alex Chan <alex@alexwlchan.net>
2023-05-07 21:59:54 +0100

    start fleshing out the Swift scripts

---
+++ get_photos.swift
@@ -0,0 +1,55 @@
+#!/usr/bin/env swift
+// Print a list of album names that contain this photo.
+//
+// This takes one arguments: the UUID of the photo.
+//
+// == Usage ==
+//
+//    $ swift get_albums_for_photo.swift 9D28ABBE-79F6-402F-8750-8674840EDA3D
+//    ["Flagged"]
+//
+
+import Photos
+
+let options = PHFetchOptions()
+options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+options.fetchLimit = 10
+
+let assets = PHAsset.fetchAssets(with: options)
+print(assets.count)
+
+print(assets.firstObject!)
+
+// func getPhotoWith(uuid: String) -> PHAsset {
+//   let fetchResult = PHAsset.fetchAssets(withLocalIdentifiers: [uuid], options: nil)
+//
+//   if fetchResult.count == 1 {
+//     return fetchResult.firstObject!
+//   } else {
+//     fputs("Unable to find photo with ID: \(uuid).\n", stderr)
+//     exit(1)
+//   }
+// }
+//
+// let arguments = CommandLine.arguments
+//
+// if arguments.count != 1 {
+//   fputs("Usage: \(arguments[0]) PHOTO_ID\n", stderr)
+//   exit(1)
+// }
+//
+// let photo = getPhotoWith(uuid: arguments[1])
+//
+// let collections = PHAssetCollection.fetchAssetCollectionsContaining(
+//   photo, with: .album, options: nil
+// )
+//
+// var titles: [String] = []
+//
+// collections.enumerateObjects({ (album, index, stop) in
+//   if (album.localizedTitle != nil) {
+//     titles.append(album.localizedTitle!)
+//   }
+// })
+//
+// print(titles)
