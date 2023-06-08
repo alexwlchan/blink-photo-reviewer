2b64a0ef4fb73ba07b2ea5e5597166b8904e9763
Alex Chan <alex@alexwlchan.net>
2023-06-08 10:53:23 +0100

    Pull this view out into a standalone file

--- BlinkReviewer/BlinkReviewer/Views/PhotoReviewer.swift
+++ BlinkReviewer/BlinkReviewer/Views/PhotoReviewer.swift
@@ -15,33 +15,9 @@ struct PhotoReviewer: View {
     var body: some View {
         VStack {
             Divider()
-            ScrollViewReader { proxy in
-                ScrollView(.horizontal) {
-                    LazyHStack(spacing: 5) {
-                        // TODO: placeholder images for start/end
-                        // TODO: Allow tapping thumbnails to jump to that
-                        
-                        // Implementation note: we use the localIdentifier rather than the
-                        // array index as the id here, because the app gets way slower if
-                        // you use the array index -- it tries to regenerate a bunch of
-                        // the thumbnails every time you change position.
-                        ForEach(Array(assets.enumerated()), id: \.element.localIdentifier) { index, asset in
-                            ThumbnailImage(
-                                thumbnail: asset.getThumbnail(),
-                                isSelected: assets[selectedAssetIndex].localIdentifier == asset.localIdentifier
-                            ).onTapGesture {
-                                selectedAssetIndex = index
-                            }
-                        }
-                    }.padding()
-                }.frame(height: 70)
-                    .onChange(of: selectedAssetIndex, perform: { newIndex in
-                        withAnimation {
-                            proxy.scrollTo(assets[newIndex].localIdentifier, anchor: .center)
-                        }
-                        
-                    })
-            }
+            
+            ThumbnailList(assets: assets, selectedAssetIndex: $selectedAssetIndex)
+            
             Divider()
             
             PreviewImage(asset: assets[selectedAssetIndex])
