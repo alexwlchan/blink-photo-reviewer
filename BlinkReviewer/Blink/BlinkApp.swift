097102be218261423f0f8896be82edd6f90110ab
Alex Chan <alex@alexwlchan.net>
2023-06-17 08:35:42 +0100

    the app only has and needs a single window

--- BlinkReviewer/Blink/BlinkApp.swift
+++ BlinkReviewer/Blink/BlinkApp.swift
@@ -10,9 +10,15 @@ import SwiftUI
 @main
 struct BlinkApp: App {
     let photosLibrary = PhotosLibrary()
-    
+        
     var body: some Scene {
-        WindowGroup {
+        // Note: this uses `Window` instead of the `WindowGroup` from the
+        // standard SwiftUI template, so that SwiftUI knows this app only
+        // ever needs a single window, and it doesn't need to offer
+        // window/tab management.
+        //
+        // See https://www.optionalmap.com/posts/swiftui_single_window_app/
+        Window("Blink", id: "main") {
             PhotoReviewer().environmentObject(photosLibrary)
         }
     }
